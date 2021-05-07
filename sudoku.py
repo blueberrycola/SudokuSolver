5c15faf98021a637217b6dc7a1bcbf5a0b595228
Chase Johnston <46426781+blueberrycola@users.noreply.github.com>
2021-05-06 20:28:22 -0400

    countcube func added

--- sudoku.py
+++ sudoku.py
@@ -3,7 +3,10 @@
 ###     Version 0.1           ###
 
 #TODO:
-    #Create a function to print the game object
+    #Create a function to print the game object X
+    #Create a function to load various sudoku puzzles from text X
+    #Create function to solve sudoku
+    #Create function to check horizontally, vertically, and by each cell.
 
 #Game variables
 cellformat = "|  | " #Used to make a cell in sudoku, use index 2 to replace with num
@@ -42,14 +45,76 @@ def printgame():
         i += 1
         print()
         if(i == 3 or i == 6):
-            print("- - - - - - - - - - - - - - - - - - - - - - - - -")
-
-
-
+            print("= - - - - - - - + - - - - - - - - + - - - - - - - =")
+#Returns array that contain the count for how many nums are already filled. Used to find the one with the most numbers
+def countcube(cubeindex):
+    upper = [0, 1, 2]
+    mid = [3, 4, 5]
+    lower = [6, 7, 8]
+    count = 0
+    
+    if(cubeindex == 0):
+        for num in upper:
+            for num2 in upper:
+                if(game[num][num2] != '0'):
+                    count += 1
+    if(cubeindex == 1):
+        for num in upper:
+            for num2 in mid:
+                if(game[num][num2] != '0'):
+                    count += 1
+    if(cubeindex == 2):
+        for num in upper:
+            for num2 in lower:
+                if(game[num][num2] != '0'):
+                    count += 1
+    if(cubeindex == 3):
+        for i in mid:
+            for j in upper:
+                if(game[i][j] != '0'):
+                    count += 1
+    if(cubeindex == 4):
+        for i in mid:
+            for j in mid:
+                if(game[i][j] != '0'):
+                    count += 1
+    if(cubeindex == 5):
+        for i in mid:
+            for j in lower:
+                if(game[i][j] != '0'):
+                    count += 1
+    if(cubeindex == 6):
+        for i in lower:
+            for j in upper:
+                if(game[i][j] != '0'):
+                    count += 1
+    if(cubeindex == 7):
+        for i in lower:
+            for j in mid:
+                if(game[i][j] != '0'):
+                    count += 1
+    if(cubeindex == 8):
+        for i in lower:
+            for j in lower:
+                if(game[i][j] != '0'):
+                    count += 1
+    
+    return count                
+    
         
 def main():
     loadgame()
     printgame()
+    print(countcube(0))
+    print(countcube(1))
+    print(countcube(2))
+    print(countcube(3))
+    print(countcube(4))
+    print(countcube(5))
+    print(countcube(6))
+    print(countcube(7))
+    print(countcube(8))
+    
     
     
 main()
