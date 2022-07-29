f0b0080327561cfec361093f3c70c9656a68520c
Chase J <46426781+blueberrycola@users.noreply.github.com>
2022-07-29 14:28:09 -0400

    permutation function
    Why guess each individual number when you can do the whole row?

--- src/SudukoSolver.py
+++ src/SudukoSolver.py
@@ -1,22 +1,25 @@
 from numpy import array, genfromtxt
-#TODO:                                              (cell)
-#   Create Puzzle Object, fmt str                   |_|_|_|  <---- Row ---->
-#   Create seeRow, seeCol, and seeCell              |_|_|_|  ^
-#   Find some backtracking algos to try             |_|_|_|  | (col)
-#   Implement testing                                                         v
-#   fmt str: 
-        #getrow
-        #getcol
-        #getcube
-#
-#
+from itertools import permutations
+#                  (cell)
+#                 |_|_|_|  <---- Row ---->
+#                 |_|_|_|  ^
+#                 |_|_|_|  | (col)
+#                          v
+
+# The puzzle class is responsible for taking a data entry from
+# sudoku.csv found from kaggle.com and convert it into a readable 2d
+# array. Once it becomes a 2d array it is then solved through
+# backtracking and permutations
 
 class Puzzle:
+    #!!! Possible temporary data. 
+    # Used to see what is going on for permli()
+
     #Read testcase and fill array w data
     def __init__(self, basestr):
         #Create 2d list for sudoku game
         self.arr = [[0 for i in range(9)] for j in range(9)]
-        self.basestr = basestr #Always 81 chars total
+        self.basestr = basestr #Base String is ALWAYS 81 chars
         #i,j => (row, col)
         i = 0
         j = 0
@@ -27,14 +30,16 @@ class Puzzle:
                 i += 1
             else:
                 j += 1
-    def getarr(self): #return entire 2d array
+    def getarr(self): #return entire 2d array inside puzzle obj
         return self.arr
-    def getrow(self, n): #return a specified row
+    #Returns array of numbers for a specified row (0-8)
+    def getrow(self, n):
         if (n > 8 or n < 0):
             print('invalid param: seerow')
         else:
             return self.arr[n]
-    def getcol(self, n): #return a specified column
+    #Returns array of numbers for a specified column (0-8)
+    def getcol(self, n): 
         if (n > 8 or n < 0):
             print('invalid param: seecol')
         else:
@@ -42,15 +47,16 @@ class Puzzle:
             for iter in range(9):
                 temp.append(self.arr[iter][n])
             return temp
+    #Returns numbers contained in one of the sudoku cubes (0-8)
     def getcube(self, n):
-
-        coordList = []
+        coordList = [] #Takes zero, one, two
         zero = [0,1,2]
         one = [3,4,5]
         two = [6,7,8]
         coordList.append(zero)
         coordList.append(one)
         coordList.append(two)
+        #Used to print get values displayed inside a specific cube: arr(i,j)
         i = []
         j = []
         if(n >= 0 and n <= 2): #upper cubes
@@ -81,6 +87,24 @@ class Puzzle:
             for element in j:
                 temp.append(self.arr[e][element])
         return temp
+    #Will permute a list of numbers, n = row number,
+    def permuteli(self, n):
+        standard = ['1','2','3','4','5','6','7','8','9']    #standard - row = list used to solve row via permutation
+        row = self.getrow(n)
+        #remove matching nums for standard and row
+        for element in row:
+            if element != '0':
+                standard.remove(element)
+        perm = permutations(standard, len(standard))    #Create all possible permutations
+        for i in perm:
+            print(i)
+            
+            
+            
+                
+
+            
+
         
         
 
@@ -91,9 +115,6 @@ def main():
     #answerkey = genfromtxt('../resources/sudoku.csv', dtype='str', delimiter=',', usecols=1, skip_header = 1)
 
     puzzle = Puzzle(testcases[0])
-    cube = puzzle.getcube(0)
-    print(cube)
-    cubetwo = puzzle.getcube(1)
-    print(cubetwo)
-
+    p = puzzle.permuteli(0)
+    
 main()
